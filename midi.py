1df048e3dc1b183a87f4aac8143aeabe5605c535
Yrueii <aterlaze@gmail.com>
2024-08-06 13:20:02 +0700

    Update midi.py

--- midi.py
+++ midi.py
@@ -4,6 +4,7 @@ from mido import MidiFile
 import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
+from tkinter import PhotoImage
 
 def run(midi_file,location_x,location_y,track_number,sfx,volume,pitch,output):
     global f_code
@@ -17,13 +18,11 @@ def run(midi_file,location_x,location_y,track_number,sfx,volume,pitch,output):
                 break
     ms_per_beat = tempo / 1000
     ms_per_tick = ms_per_beat / ticks_per_beat
-
     switch_x = location_x - 1
     switch_y = location_y
     code = (f'Vars.world.tile({location_x},{location_y}).setNet(Blocks.worldProcessor,Team.sharded,0);'
             f'Vars.world.tile({location_x},{location_y}).build.links.add(new LogicBlock.LogicLink({switch_x}, {switch_y}, "switch1", true));'
             f'Vars.world.tile({location_x},{location_y}).build.code="sensor s switch1 @enabled;jump 0 equal s 0;set start @time;')
-
 
     f_code = ''
     track = midi_file.tracks[track_number]
@@ -48,22 +47,24 @@ def run(midi_file,location_x,location_y,track_number,sfx,volume,pitch,output):
     f_code += code
     f_code = f_code + f'; Vars.world.tile({switch_x},{switch_y}).setNet(Blocks.switchBlock,Team.sharded,1)'
     #pyperclip.copy(f_code)
-    print("JS code copied to Clipboard!"
-          "\nPaste into the game console")
 
     num_characters = len(f_code)
-    print(num_characters)
     truncated_text = f_code[:10000]
-    print(truncated_text)
     output.config(state='normal')
     output.insert(tk.END, truncated_text)
     output.config(state='disabled')
     total_characters.config(text=f'total characters : {num_characters}')
 
+
 def on_button_click():
-    midi_file = file.get()
-    midi_file = mido.MidiFile(midi_file.replace("\\", "/").replace('"', ''))
     inv = 0
+    midi_file = file.get()
+    try:
+        midi_file = mido.MidiFile(midi_file.replace("\\", "/").replace('"', ''))
+        invalidf.config(fg='green', text='valid file location!')
+    except Exception:
+        invalidf.config(fg='red', text='invalid file location!')
+        inv = 1
 
     location_x = file1.get()
     if location_x.isdigit():
@@ -128,6 +129,11 @@ root.geometry("800x500")
 root.resizable(False, False)
 root.config(bg='#323740')
 
+icon = PhotoImage(file='E:/coding/pythonProject3/micro-processor.png')
+root.iconphoto(True, icon)
+
+invalidf = tk.Label(root,text="", bg='#323740', fg='white', font=(12))
+invalidf.place(x=430, y=50)
 invalid = tk.Label(root,text="enter a number", bg='#323740', fg='white', font=(12))
 invalid.place(x=175, y=80)
 invalid1 = tk.Label(root, text="enter a number", bg='#323740', fg='white', font=(12))
@@ -146,16 +152,21 @@ tk.Label(root, text="Track Number :",bg='#323740', fg='white',font=(12)).place(x
 tk.Label(root, text="Sfx :",bg='#323740', fg='white',font=(12)).place(x=79, y=170)
 tk.Label(root, text="Volume :",bg='#323740', fg='white',font=(12)).place(x=47, y=200)
 tk.Label(root, text="Pitch :",bg='#323740', fg='white',font=(12)).place(x=65, y=230)
-tk.Label(root, text="Output :",bg='#323740', fg='white',font=(12)).place(x=300, y=73)
+tk.Label(root, text="Output :   (output is truncated, use the copy button to copy)",bg='#323740', fg='white',font=(12)).place(x=300, y=73)
 total_characters = tk.Label(root, text="total characters :",bg='#323740', fg='white',font=(12))
 total_characters.place(x=300, y=300)
-copied = tk.Label(root, text="asdf",bg='#323740', fg='white',font=(15))
+copied = tk.Label(root, text="",bg='#323740', fg='white',font=(15))
 copied.place(x=550, y=330)
+tk.Label(root, text="File : File location of your midi file",bg='#323740', fg='white',font=(12)).place(x=30, y=330)
+tk.Label(root, text="eg C:/Users/user-name/Downloads/cat.midi",bg='#323740', fg='yellow',font=(12)).place(x=270, y=330)
 
+tk.Label(root, text="Location X : the X coordinates of where you want the processors to be",bg='#323740', fg='white',font=(12)).place(x=30, y=350)
+tk.Label(root, text="Location Y : the Y coordinates of where you want the processors to be",bg='#323740', fg='white',font=(12)).place(x=30, y=370)
+tk.Label(root, text="Track Number : Some midi files has multiple tracks(the different colored notes you see on a synthesizer app)\n 0 being the first tracks",bg='#323740', fg='white',font=(12)).place(x=30, y=390)
+tk.Label(root, text="pitch : the chosen sfx default pitch (60 is c4)",bg='#323740', fg='white',font=(12)).place(x=30, y=430)
 
 file = ttk.Entry(root, style='TEntry', font=(10))
 file.place(x=120, y=50, width=300, height=23)
-file.insert(0, 'F:/Download (F)/UNDERTALE - ASGORE (Asgores Fight Theme) tracks combined.mid')
 
 file1 = ttk.Entry(root, style='TEntry', font=(10) )
 file1.place(x=120, y=80, width=50, height=23)
@@ -192,12 +203,13 @@ copy = tk.Button(root, text="Copy!", command=copy,font=(13))
 copy.place(x=550, y=300,width=80,height=30)
 
 
+# Run the application
 root.mainloop()
 
 
 
 
-# @sfx-click
+#@sfx-click
 # @sfx-press 60
 # @sfx-chatMessage 91
 # @sfx-noammo 40
